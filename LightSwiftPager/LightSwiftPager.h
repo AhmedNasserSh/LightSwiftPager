71cca73fc4badec9c463f522037a8e5be19969eb
Ahmed Nasser <ahmed.nasser2310@gmail.com>
2018-06-01 14:23:23 +0200

    Initial Commit

---
+++ LightSwiftPager/LightSwiftPager.h
@@ -0,0 +1,19 @@
+//
+//  LightSwiftPager.h
+//  LightSwiftPager
+//
+//  Created by AHMED NASSER on 6/1/18.
+//  Copyright © 2018 AHMED NASSER. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LightSwiftPager.
+FOUNDATION_EXPORT double LightSwiftPagerVersionNumber;
+
+//! Project version string for LightSwiftPager.
+FOUNDATION_EXPORT const unsigned char LightSwiftPagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LightSwiftPager/PublicHeader.h>
+
+
